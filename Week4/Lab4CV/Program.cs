65b31360d99db3e9ef35c6b34393fc98c9d4699e
Casey Viens <casey.viens@gmail.com>
2020-08-14 19:06:53 -0400

    Lab 4 | Part 3

--- Week4/Lab4CV/Program.cs
+++ Week4/Lab4CV/Program.cs
@@ -1,53 +1,191 @@
-﻿using System;
+﻿//Casey Viens
+//Lab 4~~~Creates a private class of Person which a user can use to store contact information in.
+//Makes use of Gets/Sets and validation on certain variables.
+using System;
 
 namespace Lab4CV
 {
     public class Person
     {
-        public string firstName;
-        public string middleName;
-        public string lastName;
-        public string street1;
-        public string street2;
-        public string city;
-        public string state;
-        public string zip;
-        public string phone;
-        public string email;
+        private string firstName;
+        private string middleName;
+        private string lastName;
+        private string street1;
+        private string street2;
+        private string city;
+        private string state;
+        private string zip;
+        private string phone;
+        private string email;
 
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                //checks the data in contact.FirstName and removes "Poopy" if it is in the string
+                firstName = value.Replace("Poopy","");
+            }
+        }
+        public string MiddleName
+        {
+            get
+            {
+                return middleName;
+            }
+            set
+            {
+                middleName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+        public string Street1
+        {
+            get
+            {
+                return street1;
+            }
+            set
+            {
+                street1 = value;
+            }
+        }
+        public string Street2
+        {
+            get
+            {
+                return street2;
+            }
+            set
+            {
+                street2 = value;
+            }
+        }
+        public string City
+        {
+            get
+            {
+                return city;
+            }
+            set
+            {
+                city = value;
+            }
+        }
+        public string State
+        {
+            get
+            {
+                return state;
+            }
+            set
+            {
+                state = value;
+            }
+        }
+        public string Zip
+        {
+            get
+            {
+                return zip;
+            }
+            set
+            {
+                //Ensures the data in contact.Zip is equal to 5 digits and therefore not an areacode
+                if (value.Length == 5)
+                {
+                    zip = value;
+                }
+                else
+                {
+                    zip = "Invalid";
+                }
+            }
+        }
+        public string Phone
+        {
+            get
+            {
+                return phone;
+            }
+            set
+            {
+                //Removes all "-" from the data in contact.Phone
+                phone = value.Replace("-","");
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                //Ensures the email entered is valid based on the 2@2.2 rule
+                int atLocation = value.IndexOf("@");
+                int periodLocation = value.LastIndexOf(".");
+                if (value.Length >= 8 && atLocation >= 2 && (periodLocation + 2) < value.Length)
+                {
+                    email = value;
+                }
+                else
+                {
+                    email = "Invalid";
+                }
+            }
+        }
         public void Print()
         {
-            Console.WriteLine($"\nFirst Name: {this.firstName}\nMiddle Name: {this.middleName}\nLast Name: {this.lastName}\nStreet 1: {this.street1}\nStreet 2: {this.street2}\nCity: {this.city}\nState: {this.state}\nZip: {this.zip}\nPhone: {this.phone}\nEmail: {this.email}");
+            //Prints all of the data stored in the Person class
+            Console.WriteLine($"\nFirst Name: {this.FirstName}\nMiddle Name: {this.MiddleName}\nLast Name: {this.LastName}\nStreet 1: {this.Street1}\nStreet 2: {this.Street2}\nCity: {this.City}\nState: {this.State}\nZip: {this.Zip}\nPhone: {this.Phone}\nEmail: {this.Email}");
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
+            //creates and instance of person
             Person contact = new Person();
-            
+            //Allows the user to add data to Person
             Console.Write("Please enter the person's first name: ");
-            contact.firstName = Console.ReadLine() + "Poopy";
+            contact.FirstName = Console.ReadLine() + "Poopy";
             Console.Write("Please enter the person's middle name: ");
-            contact.middleName = Console.ReadLine();
+            contact.MiddleName = Console.ReadLine();
             Console.Write("Please enter the person's last name: ");
-            contact.lastName = Console.ReadLine();
+            contact.LastName = Console.ReadLine();
             Console.Write("Please enter the person's street1: ");
-            contact.street1 = Console.ReadLine();
+            contact.Street1 = Console.ReadLine();
             Console.Write("Please enter the person's street2: ");
-            contact.street2 = Console.ReadLine();
+            contact.Street2 = Console.ReadLine();
             Console.Write("Please enter the person's city: ");
-            contact.city = Console.ReadLine();
+            contact.City = Console.ReadLine();
             Console.Write("Please enter the person's state: ");
-            contact.state = Console.ReadLine();
+            contact.State = Console.ReadLine();
             Console.Write("Please enter the person's zip: ");
-            contact.zip = Console.ReadLine();
+            contact.Zip = Console.ReadLine();
             Console.Write("Please enter the person's phone: ");
-            contact.phone = Console.ReadLine();
+            contact.Phone = Console.ReadLine();
             Console.Write("Please enter the person's email: ");
-            contact.email = Console.ReadLine();
-
+            contact.Email = Console.ReadLine();
+            //calls the print function
             contact.Print();
+            //Pauses the program so the user can read the data
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
         }
     }
 }
