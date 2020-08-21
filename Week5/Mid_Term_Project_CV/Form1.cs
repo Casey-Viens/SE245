8666326ced14b1631e29e0d5251f4e16a5a45f7f
Casey Viens <casey.viens@gmail.com>
2020-08-21 19:42:42 -0400

    Mid Term Project | Contact Info Form

---
+++ Week5/Mid_Term_Project_CV/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mid_Term_Project_CV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Person contact = new Person();
+
+            contact.FirstName = txtFirstName.Text;           
+            contact.MiddleName = txtMiddleName.Text;           
+            contact.LastName = txtLastName.Text;
+            contact.Street1 = txtStreet1.Text;           
+            contact.Street2 = txtStreet2.Text;          
+            contact.City = txtCity.Text;          
+            contact.State = txtState.Text;          
+            contact.Zip = txtZip.Text;          
+            contact.Phone = txtPhone.Text;        
+            contact.Email = txtEmail.Text;
+
+            if (contact.Feedback.Contains("Error:"))
+            {
+                lblFeedback.Text = contact.Feedback;
+            }
+            else
+            {
+                lblFeedback.Text = $"Contact for {contact.FirstName} {contact.LastName} was created successfully.";
+            }
+        }
+    }
+}
---
+++ Week5/Mid_Term_Project_CV/Person_Class.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mid_Term_Project_CV
+{
+    public class Person
+    {
+        private string firstName;
+        private string middleName;
+        private string lastName;
+        private string street1;
+        private string street2;
+        private string city;
+        private string state;
+        private string zip;
+        private string phone;
+        private string email;
+        private string feedback = "";
+
+        public string Feedback
+        {
+            get
+            {
+                return feedback;
+            }
+            set
+            {
+                feedback = value;
+            }
+        }
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+        public string MiddleName
+        {
+            get
+            {
+                return middleName;
+            }
+            set
+            {
+                middleName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+        public string Street1
+        {
+            get
+            {
+                return street1;
+            }
+            set
+            {
+                street1 = value;
+            }
+        }
+        public string Street2
+        {
+            get
+            {
+                return street2;
+            }
+            set
+            {
+                street2 = value;
+            }
+        }
+        public string City
+        {
+            get
+            {
+                return city;
+            }
+            set
+            {
+                city = value;
+            }
+        }
+        public string State
+        {
+            get
+            {
+                return state;
+            }
+            set
+            {
+                if (value.Length == 2)
+                {
+                    state = value;
+                }
+                else
+                {
+                    Feedback = "Error: The State needs to be abbreviated.";
+                }
+            }
+        }
+        public string Zip
+        {
+            get
+            {
+                return zip;
+            }
+            set
+            {
+                //Ensures the data in contact.Zip is equal to 5 digits and therefore not an areacode
+                if (value.Length == 5)
+                {
+                    zip = value;
+                }
+                else
+                {
+                    Feedback = "Error: The zipcode needs to be 5 digits.";
+                }
+            }
+        }
+        public string Phone
+        {
+            get
+            {
+                return phone;
+            }
+            set
+            {
+                //Removes all "-" from the data in contact.Phone
+                phone = value.Replace("-", "");
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                //Ensures the email entered is valid based on the 2@2.2 rule
+                int atLocation = value.IndexOf("@");
+                int periodLocation = value.LastIndexOf(".");
+                if (value.Length >= 8 && atLocation >= 2 && (periodLocation + 2) < value.Length)
+                {
+                    email = value;
+                }
+                else
+                {
+                    Feedback = "Error: The email entered does not meet the 2@2.2 format.";
+                }
+            }
+        }
+    }
+}
